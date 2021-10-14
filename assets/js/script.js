12521b06cdea3a9ffb7533367da98e16669601ff
jj77847 <81172135+jj77847@users.noreply.github.com>
2021-10-14 21:42:15 +0100

    layout changes

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+var repoList = document.querySelector("ul");
+var fetchButton = document.getElementById("fetch-button");
+
+//getApi function is called when the fetchButton is clicked
+
+function getApi() {
+  // Insert the API url to get a list of your repos
+  var requestUrl = "https://api.github.com/users/jj77847/repos";
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      //looping over the fetch response and inserting the URL of your repos into a list
+      for (var i = 0; i < data.length; i++) {
+        //Create a list element
+        var listItem = document.createElement("li");
+
+        //Set the text of the list element to the JSON response's .html_url property
+        listItem.textContent = data[i].html_url;
+
+        //Append the li element to the id associated with the ul element.
+        repoList.appendChild(listItem);
+      }
+    });
+}
+
+fetchButton.addEventListener("click", getApi);
+
+// const container = $("#card-container");
+// const create
+
+{
+  /* <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div> */
+}
